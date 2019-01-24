4291785db0ce911343aef42efe9597821de56e0c
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-24 11:13:47 +0700

    Add merging component state

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,27 +2,43 @@ import React, { Component } from 'react';
 
 class MyComponent extends Component {
   state = {
-    heading: 'React awesomesauce {busy}',
-    content: 'loading...',
+    first: 'loading...',
+    second: 'loading...',
+    third: 'loading...',
+    fourth: 'loading...',
+    doneMessage: 'finished',
   };
 
   render() {
-    const { heading, content } = this.state;
+    const { state } = this;
 
     setTimeout(() => {
-      this.setState({
-        heading: 'React Awesomesauce',
-        content: 'Done!',
-      });
+      this.setState({ first: 'done!' });
+    }, 1000);
+    setTimeout(() => {
+      this.setState({ second: 'done!' });
+    }, 2000);
+    setTimeout(() => {
+      this.setState({ third: 'done!' });
     }, 3000);
+    setTimeout(() => {
+      this.setState(() => ({
+        ...state,
+        fourth: state.doneMessage,
+      }));
+    }, 4000);
 
     return (
-      <main>
-        <h1>{heading}</h1>
-        <p>
-          {content}
-        </p>
-      </main>
+      <ul>
+        {Object.keys(state)
+          .filter(key => key !== 'doneMessage')
+          .map(key => (
+            <li key={key}>
+              <strong>{key}: </strong>
+              {state[key]}
+            </li>
+          ))}
+      </ul>
     );
   }
 }
