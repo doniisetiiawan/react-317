5819dda7e928853884e54f39c44321dc31f6f879
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-27 11:58:14 +0700

    Refactor making components functional

--- src/AddArticle.jsx
+++ src/AddArticle.jsx
@@ -1,54 +1,26 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function createLog(log) {
-  return () => console.warn(`${log} is not defined`);
-}
-
-function AddArticle(props) {
-  const {
-    name,
-    title,
-    summary,
-    onChangeTitle,
-    onChangeSummary,
-    onClickAdd,
-  } = props;
-
-  return (
-    <header>
-      <h1>{name}</h1>
-      <input
-        placeholder="Title"
-        value={title}
-        onChange={onChangeTitle}
-      />
-      <input
-        placeholder="Summary"
-        value={summary}
-        onChange={onChangeSummary}
-      />
-      <button type="button" onClick={onClickAdd}>Add</button>
-    </header>
-  );
-}
-
-AddArticle.propTypes = {
-  name: PropTypes.string,
-  onChangeSummary: PropTypes.func,
-  onChangeTitle: PropTypes.func,
-  onClickAdd: PropTypes.func,
-  summary: PropTypes.string,
-  title: PropTypes.string,
-};
-
-AddArticle.defaultProps = {
-  name: 'Articles',
-  onChangeSummary: createLog('onChangeSummary'),
-  onChangeTitle: createLog('onChangeTitle'),
-  onClickAdd: createLog('onClickAdd'),
-  summary: 'summary',
-  title: 'title',
-};
-
-export default AddArticle;
+export default ({
+  name,
+  title,
+  summary,
+  onChangeTitle,
+  onChangeSummary,
+  onClickAdd,
+}) => (
+  <header>
+    <h1>{name}</h1>
+    <input
+      placeholder="Title"
+      value={title}
+      onChange={onChangeTitle}
+    />
+    <input
+      placeholder="Summary"
+      value={summary}
+      onChange={onChangeSummary}
+    />
+    <button type="button" onClick={onClickAdd}>Add</button>
+  </header>
+);
--- src/ArticleList.jsx
+++ src/ArticleList.jsx
@@ -1,38 +1,17 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import ArticleItem from './ArticleItem';
 
-function createLog(log) {
-  return () => console.warn(`${log}is not defined`);
-}
-
-function ArticleList(props) {
-  const { articles, onClickToggle, onClickRemove } = props;
-  return (
-    <ul>
-      {articles.map(article => (
-        <ArticleItem
-          key={article.id}
-          article={article}
-          onClickToggle={onClickToggle}
-          onClickRemove={onClickRemove}
-        />
-      ))}
-    </ul>
-  );
-}
-
-ArticleList.propTypes = {
-  articles: PropTypes.arrayOf(PropTypes.object),
-  onClickRemove: PropTypes.func,
-  onClickToggle: PropTypes.func,
-};
-
-ArticleList.defaultProps = {
-  articles: [],
-  onClickRemove: createLog('onClickRemove'),
-  onClickToggle: createLog('onClickToggle'),
-};
-
-export default ArticleList;
+export default ({ articles, onClickToggle, onClickRemove }) => (
+  <ul>
+    {articles.map(article => (
+      <ArticleItem
+        key={article.id}
+        article={article}
+        onClickToggle={onClickToggle}
+        onClickRemove={onClickRemove}
+      />
+    ))}
+  </ul>
+);
