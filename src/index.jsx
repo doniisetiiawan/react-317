5812a72f48695f1b7f18b44133f0c4db79680485
Doni Setiawan <donisetiawan.050@gmail.com>
2019-02-03 14:56:25 +0700

    Add conditional component rendering

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import cond from './cond';
 import MyComponent from './MyComponent';
+
+const ComposedVisible = cond(MyComponent, () => true);
+const ComposedHidden = cond(MyComponent, () => false);
 
 ReactDOM.render(
   <section>
-    <MyComponent />
+    <h1>Visible</h1>
+    <ComposedVisible />
+    <h1>Hidden</h1>
+    <ComposedHidden />
   </section>,
   document.getElementById('root'),
 );
